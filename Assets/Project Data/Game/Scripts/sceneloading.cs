c6acc85775628a2fa944c6a47561b1e2be346cb2
Mohammadd Fahad Siddiqui <mohdfahadsiddiqui1248@gmail.com>
2026-09-14 01:13:02 +0530

    Stop duplicate EventSystem spam in redesigned menu

--- Assets/Project Data/Game/Scripts/sceneloading.cs
+++ Assets/Project Data/Game/Scripts/sceneloading.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 namespace Watermelon.BusStop
@@ -19,6 +21,61 @@ namespace Watermelon.BusStop
                     redesign = gameObject.AddComponent<MainMenuRedesignController>();
 
                 redesign.Initialise(this);
+                SanitiseMenuRuntime();
+            }
+        }
+
+        private void SanitiseMenuRuntime()
+        {
+            // The menu scene already owns an EventSystem. The redesign must never leave
+            // more than one enabled EventSystem because Unity logs that warning every frame.
+            EventSystem[] eventSystems = FindObjectsByType<EventSystem>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+            EventSystem keeper = EventSystem.current;
+            if (keeper == null || !keeper.enabled || !keeper.gameObject.activeInHierarchy)
+            {
+                keeper = null;
+
+                for (int i = 0; i < eventSystems.Length; i++)
+                {
+                    EventSystem candidate = eventSystems[i];
+                    if (candidate != null && candidate.enabled && candidate.gameObject.activeInHierarchy)
+                    {
+                        keeper = candidate;
+                        break;
+                    }
+                }
+            }
+
+            if (keeper == null && eventSystems.Length > 0)
+            {
+                keeper = eventSystems[0];
+                keeper.enabled = true;
+            }
+
+            for (int i = 0; i < eventSystems.Length; i++)
+            {
+                EventSystem eventSystem = eventSystems[i];
+                if (eventSystem == null || eventSystem == keeper)
+                    continue;
+
+                eventSystem.enabled = false;
+
+                BaseInputModule[] inputModules = eventSystem.GetComponents<BaseInputModule>();
+                for (int j = 0; j < inputModules.Length; j++)
+                    inputModules[j].enabled = false;
+            }
+
+            // FredokaOne does not contain the triangular play glyph used by the redesign.
+            // Replace it before the first normal menu render so TMP does not spam warnings.
+            TextMeshProUGUI[] labels = FindObjectsByType<TextMeshProUGUI>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+            for (int i = 0; i < labels.Length; i++)
+            {
+                if (labels[i] != null && labels[i].name == "PlayLabel")
+                {
+                    labels[i].text = "PLAY";
+                    break;
+                }
             }
         }
 
