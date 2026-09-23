2e00e66491211e52b5bc0e06a989eef711322aa5
Mohammadd Fahad Siddiqui <mohdfahadsiddiqui1248@gmail.com>
2026-09-23 23:13:43 +0530

    Bump Level Selection authored layout to v3

--- Assets/Project Data/Game/Scripts/UI/LevelSelection/LevelSelectionResponsiveLayout.cs
+++ Assets/Project Data/Game/Scripts/UI/LevelSelection/LevelSelectionResponsiveLayout.cs
@@ -11,7 +11,7 @@ namespace Watermelon
     public sealed class LevelSelectionResponsiveLayout : MonoBehaviour
     {
         [SerializeField] private Vector2 referenceResolution = new Vector2(1080f, 1920f);
-        [SerializeField] private int layoutVersion = 2;
+        [SerializeField] private int layoutVersion = 3;
 
         public Vector2 ReferenceResolution => referenceResolution;
         public int LayoutVersion => layoutVersion;
